48e701812de2ef129fd8574da8fcd36267857eba
Mackenzie Wade <mackenziewade@mackenzies-air.wireless.rit.edu>
2023-09-08 13:40:20 -0400

    project created

---
+++ Hedera Management App/Hedera Management App/Hedera_Management_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hedera_Management_AppApp.swift
+//  Hedera Management App
+//
+//  Created by Mackenzie Wade on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Hedera_Management_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
